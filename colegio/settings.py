f354d33129a4e6b63815fd7c9758147847ab32f4
vicente farias <vic.fariasm@duocuc.cl>
2023-10-20 16:18:09 -0300

    commit 3

--- colegio/settings.py
+++ colegio/settings.py
@@ -294,7 +294,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 ALLOWED_HOSTS = ['.vercel.app' , 'now.sh','127.0.0.1', 'localhost']
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATICFILES_DIRS = os.path.join(BASE_DIR, './proyectoapp/static'),
 
